6aab98ac0389c47178ac1f21dac162f037f4704c
Anshul Agrawal <anshul.agrawal@libertymutual.com>
2024-05-01 00:36:24 -0400

    Regiser new user front-end

--- front-end/src/LoginForm.js
+++ front-end/src/LoginForm.js
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
 export function LoginForm(params) {
+
+    const [showEmailField, setShowEmailField] = useState(false);
+    const [email, setEmail] = useState('');
 
     const handleChange = (event) => {
         let newCredentials = { ...params.credentials };
@@ -6,23 +11,50 @@ export function LoginForm(params) {
         params.setCredentials(newCredentials);
     };
 
+    const handleRegisterClick = () => {
+        setShowEmailField(true);
+    };
+
+    const handleBackToLogin = () => {
+        setShowEmailField(false);
+        setEmail('');
+    };
+
     return (
         <div className="box" style={{ maxWidth: "unset" }}>
-            <button onClick={params.login}>
+            <button className={showEmailField ? "hidden" : "visible"} onClick={params.login}>
                 {(params.currentUser) ? "Logout" : "Login"}
             </button>
-            &nbsp;User: <span style={{ fontWeight: "bold" }} >{(params.currentUser) ? params.currentUser.username : "not logged in"}</span>
+            <p className={showEmailField ? "hidden" : "visible"}>
+                User: <span style={{ fontWeight: "bold" }} >{(params.currentUser) ? params.currentUser.username : "not logged in"}</span>
+            </p>
             <div className={(params.currentUser) ? "hidden" : "visible"}>
                 <div>
                     <label htmlFor="username">Username: </label>
-                    <input type="text" size={10} id="username" name="username" value={params.credentials.username} onChange={handleChange} />
+                    <input type="text" size={10} id="username" required={true} name="username" value={params.credentials.username} onChange={handleChange} />
                 </div>
 
                 <div>
                     <label htmlFor="password">Password: </label>
-                    <input type="password" size={10} id="password" name="password" value={params.credentials.password} onChange={handleChange} />
+                    <input type="password" size={10} id="password" required={true} name="password" value={params.credentials.password} onChange={handleChange} />
+                </div>
+                {showEmailField && (
+                    <div>
+                        <div>
+                            <label htmlFor="email">Email: </label>
+                            <input type="email" size={10} id="email" required={true} name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div>
+                            <button onClick={handleBackToLogin}>Back to Login</button>
+                        </div>
+                    </div>
+                )}
+                &nbsp;
+                <div>
+                    <button onClick={handleRegisterClick}>Register</button>
                 </div>
             </div>
+
         </div>
     );
 }
