06bc734725289164a0bfd8adbb84f2bb2a8e2c69
Anshul Agrawal <anshul.agrawal@libertymutual.com>
2024-05-01 13:31:03 -0400

    Show more characters for each title in articles list

--- front-end/src/Articles.js
+++ front-end/src/Articles.js
@@ -1,29 +1,26 @@
 export function Articles(params) {
-    let articles = (params.data.articles)?params.data.articles:[];
-    let queryName = (params.query.queryName)?params.query.queryName:"na";
-    let articleCount = (params.data.totalResults)?params.data.totalResults:0;
-    return (
-      <div>
-        Query: {queryName}
-        <br/>Count: {articleCount}
-        <ol >{
-            articles.map((item, idx) => {
-              if(item){
-                if(item.title){
-                  if(item.title === "[Removed]"){
-                    return (<li key={idx} >Was Removed</li>);
-                  }
-                  let trimTitle = item.title.substring(0,30);
-                  return (<li key={idx}>{trimTitle}<a href={item.url} target="_blank" rel="noreferrer" >&nbsp;Link</a></li>);    
-                }else{
-                  return (<li key={idx}>No Title</li>);
-                }
-              }else{
-                return (<li key={1} >No Item</li>);
-              }
-            })
-        }</ol>
-      </div>
-    )
-  
-  }
+  const articles = params.data.articles || [];
+  const queryName = params.query.queryName || "na";
+  const articleCount = params.data.totalResults || 0;
+
+  return (
+    <div>
+      <p>Query: {queryName}</p>
+      <p>Count: {articleCount}</p>
+      <ul className="article-list">
+        {articles.map((item, idx) => {
+          const title = item && item.title ? item.title : "No Title";
+          const trimmedTitle = title.length > 50 ? title.substring(0, 50) + "..." : title;
+
+          return (
+            <li key={idx}>
+              <a href={item.url} target="_blank" rel="noreferrer">
+                {trimmedTitle}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
